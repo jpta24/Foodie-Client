3ee1445c9b60b42d5bacbed22c45e39bdb5c71c0
Jpta24 <jeanpierretorres@gmail.com>
2022-10-05 11:58:55 +0200

    WIP BuzView

---
+++ src/components/BusinessViewCard.jsx
@@ -0,0 +1,16 @@
+import { Card, Button } from 'react-bootstrap';
+
+const BusinessViewCard = ({src,href,button}) => {
+  return (
+    <>
+        <Card className='col-4 mx-3 my-2 p-2  mx-md-5 col-md-2'>
+            <Card.Img className='p-2' variant='top' src={src} />
+            <Button variant='outline-success' size='sm' href={href}>
+                {button}
+            </Button>
+        </Card>
+    </>
+  )
+}
+
+export default BusinessViewCard
