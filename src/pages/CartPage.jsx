c37d2d3cd8f81628f2b401241e3ce85acd0cc834
Jpta24 <jeanpierretorres@gmail.com>
2022-10-07 00:28:54 +0200

    Basic Cart done

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,10 @@
 import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../context/cart.context';
 import { AuthContext } from '../context/auth.context';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 import { Button,Form } from 'react-bootstrap';
 
@@ -14,7 +17,9 @@ import iconsCloud from '../data/icons.json'
 
 const CartPage = () => {
     const { user } = useContext(AuthContext);
-    const { cart } = useContext(CartContext);
+    const { cart,setCart } = useContext(CartContext);
+	const navigate = useNavigate();
+
     const [summary, setSummary] = useState(0)
 
     const updateSummary =()=>{
@@ -32,6 +37,9 @@ const CartPage = () => {
         card: false,
         pp: false
     }
+    
+    const updatedRol = {...initialState,cash:true}
+
     const initialAddress ={
         street:'',
         name:'',
@@ -39,7 +47,7 @@ const CartPage = () => {
         phone:''
     }
 
-    const [rol, setRol] = useState(initialState)
+    const [rol, setRol] = useState(updatedRol)
     const [address, setAddress] = useState(initialAddress)
 
     const handleChange = (rolClicked) => {
@@ -47,6 +55,68 @@ const CartPage = () => {
         newState[rolClicked]= true
         // console.log(newState)
         setRol(newState)
+    }
+
+    const handlePlaceOrder =()=>{
+        const storedToken = localStorage.getItem("authToken"); 
+
+        let payMethod = ''
+        
+        Object.entries(rol).forEach(each =>{
+            if(each[1]){
+                payMethod = each[0]
+            }
+        })
+        const businessIdArr = cart.map(prod => {
+            return prod.product.business
+        });
+        const buzs = [...new Set(businessIdArr)]
+        const orders =[]
+
+        
+        buzs.forEach(buz=>{
+            return orders.push({
+                business: buz,
+                user:user._id,
+                status:'pending',
+                paymentMethod:payMethod,
+                products:[],
+                note: address,
+                summary:summary
+            })
+        })
+        orders.forEach(each=>{
+            
+            cart.forEach(elem=>{
+                if (each.business === elem.product.business) {
+                    each.products.push({
+                        product:elem.product._id,
+                        quantity:elem.quantity
+                    })
+                }
+            })
+        })
+        console.log(orders)
+
+
+		const requestBody = {
+            update:'order',
+            orders
+        } 
+
+        axios
+			.put(`${process.env.REACT_APP_SERVER_URL}/users/${user._id}`, requestBody, {headers: {Authorization: `Bearer ${storedToken}`}})
+			.then((response) => {
+		        toast.success('Order Placed', { theme: 'dark' });
+                setCart(null)
+                navigate(`/order/${user._id}`)
+			})
+			.catch((error) => {
+                console.log({error});
+				const errorDescription = error.response.data.message;
+                console.log(errorDescription);
+                toast.error('Order could not be placed', { theme: 'dark' });
+			});
     }
     
     
@@ -59,7 +129,7 @@ const CartPage = () => {
                     <div className="col-12 col-md-7">
                         <div className="col-11 d-flex flex-column p-2 mt-2 form-control">
                             <h1>Hi, {user.username}</h1>
-                            <h3>This is your Cart</h3>
+                            <h3>{cart.length===0?'Your Cart is Empty!':'This is your Cart'}</h3>
                             <div className="col-12 cartProducts">
                                 {cart.map(product => {
                                 return <CartCard key={uuidv4()} product={product} updateSummary={updateSummary}/>
@@ -68,9 +138,10 @@ const CartPage = () => {
 
                         </div>
                     </div>
-                    {window.innerWidth > 400 ? <div className="col-12 col-md-5">
-                    <div className="col-11 d-flex flex-column p-2 mt-2 py-3 form-control bg-success">
-                            <h2>Payment Method</h2>
+                    <div className="col-12 col-md-5">
+                    <div className="col-11 d-flex flex-column p-2 mt-2 py-3 form-control bg-success cartSummary">
+                            <h2> Summary: € {summary.toFixed(2)}</h2>
+                            <h4>Payment Method</h4>
                             <div className='d-flex px-2 m-2 justify-content-around'>
                                 <PayMethod onclick={()=>{handleChange('cash')}} src={rol.cash ? iconsCloud[0].userActive: iconsCloud[0].userInactive}/>
                                 <PayMethod onclick={()=>{handleChange('card')}} src={rol.card ? iconsCloud[0].adminActive: iconsCloud[0].adminInactive}/>
@@ -143,25 +214,34 @@ const CartPage = () => {
                             
 
 
-                            <h4> Summary: € {summary}</h4>
+                            
                             <div className="col-12">
-                                <Button variant='primary' size="lg" type='submit' className='mx-2 my-1 col-8 col-md-6' onClick={updateSummary}>
+                            {cart.length!==0 && <Button variant='primary' size="lg" type='submit' className='mx-2 my-1 col-8 col-md-6' onClick={handlePlaceOrder}>
                                     Place your Order!
-                                </Button>
+                                </Button>}
+                                
                                 
                             </div>
 
                         </div>
                     </div>
-                    : 
-                    <div>
-
-                    </div> }
+                    
                     
 
                 </div>
             </div>
         )
+    }else if(cart===null){
+        <div className='container p-0'>
+            <div className="row d-flex flex-row rounded border border-light">
+                <div className="col-12 col-md-7">
+                    <div className="col-11 d-flex flex-column p-2 mt-2 form-control">
+                        <h1>Hi, {user.username}</h1>
+                        <h3>Your Cart is Empty!</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     }else{
         return (
             <div>Loading...</div>
