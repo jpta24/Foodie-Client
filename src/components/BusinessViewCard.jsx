f1ab88bca74f7d93f50a6a4d8e3526edd349bd88
Jpta24 <jeanpierretorres@gmail.com>
2022-11-23 22:50:27 +0100

    Link Btns in cards done

--- src/components/BusinessViewCard.jsx
+++ src/components/BusinessViewCard.jsx
@@ -1,14 +1,15 @@
 import { Card, Button } from 'react-bootstrap';
+import {  Link  } from 'react-router-dom';
 
 const BusinessViewCard = ({src,href,button}) => {
   return (
     <>
-        <Card className='col-4 mx-0 my-2 p-2 mx-md-5 col-md-2 shadow'>
+        <Link to={href} className='card col-4 mx-0 my-2 p-2 mx-md-5 col-md-2 shadow'>
             <Card.Img className='p-2' variant='top' src={src} />
-            <Button variant='outline-success' size='sm' href={href}>
+            <Button variant='outline-success' size='sm'>
                 {button}
             </Button>
-        </Card>
+        </Link>
     </>
   )
 }
--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,14 +1,15 @@
 import { Card, Button } from 'react-bootstrap';
+import {  Link  } from 'react-router-dom'; 
 
 const DashboardCard = ({src,href,button}) => {
   return (
     <>
-        <Card className='col-4 mx-3 my-2 p-2  mx-md-5 col-md-2'>
+        <Link to={href} className=' card col-4 mx-3 my-2 p-2  mx-md-5 col-md-2'>
             <Card.Img className='p-2' variant='top' src={src} />
-            <Button variant='outline-success' size='sm' href={href}>
+            <Button variant='outline-success' size='sm'>
                 {button}
             </Button>
-        </Card>
+        </Link>
     </>
   )
 }
