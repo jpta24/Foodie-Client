bf915aa8b66dc26357b59a0b6231c17c97ddc2fd
Jpta24 <jeanpierretorres@gmail.com>
2023-06-20 14:37:02 +0200

    Buz Prod done

--- src/pages/BusinessProducts.jsx
+++ src/pages/BusinessProducts.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
 import {  useParams, useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
+import { CartContext } from '../context/cart.context';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Modal, Button } from 'react-bootstrap';
@@ -11,18 +12,24 @@ import ProductCardDesktop from '../components/ProductCardDesktop';
 
 import iconsCloud from '../data/icons.json'
 import Loading from '../components/Loading';
-import { deleteAPI, getAPI} from '../utils/api';
+import { deleteAPI, getAPI, putAPI} from '../utils/api';
 import { toastifyError } from '../utils/tostify';
+import ProductCard2 from '../components/ProductCard2';
 
 const BusinessProducts = () => {
 	const {language:lang} = useContext(AuthContext);
     const { user } = useContext(AuthContext);
+    
+	const { cart } = useContext(CartContext);
     const { businessName } = useParams();
     const navigate = useNavigate();
 
     let businessNameEncoded = businessName.split(' ').join('-')
 
     const [business, setBusiness] = useState('')
+    
+	const [userSaved, setUserSaved] = useState('');
+    
 
     const modalInitialState = {
         show:false,
@@ -35,6 +42,8 @@ const BusinessProducts = () => {
     const handleModal = (productName,productID) => {
         setShow({show:true,productName,productID})
     }
+    
+	const [modalHL, setmodalHL] = useState(false);
     const deleteProduct =()=>{
         const url = `products/delete/${show.productID}`;
         const urlGet = `business/${businessNameEncoded}`
@@ -63,6 +72,34 @@ const BusinessProducts = () => {
 		getAPI(url, thenFunction, errorFunction);
       // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [])
+      useEffect(() => {
+		if (user) {
+			const url = `users/saved/${user._id}`;
+			const thenFunction = (response) => {
+				setUserSaved(response.data);
+			};
+			const errorFunction = (error) => {
+				console.log(error);
+			};
+			getAPI(url, thenFunction, errorFunction);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [user]);
+
+    const handleSavedProductStatus = (productID) => {
+		const url = `users/savedProduct/${user._id}`;
+		const requestBody = {
+			productID: productID,
+		};
+		const thenFunction = (response) => {
+			setUserSaved(response.data);
+		};
+		const errorFunction = (error) => {
+			console.log(error);
+		};
+		putAPI(url, requestBody, thenFunction, errorFunction);
+	};
+
     
     if (business!=='') {
         if(business.owner !== user._id){
@@ -70,6 +107,37 @@ const BusinessProducts = () => {
         }
         let owner = true
         const currency = business.currency
+        const limitHL = owner ? 
+			(business.membership.plan === 'free'
+				? 3
+				: business.membership === 'basic'
+				? 10
+				: Infinity):null;
+
+        const handleHighlightedProduct = (productID) => {
+			if (
+				business.highlightedProducts.includes(productID) ||
+				business.highlightedProducts.length < limitHL
+			) {
+				const url = `business/highlightedProducts/${business._id}`;
+				const requestBody = {
+					productID: productID,
+				};
+				const thenFunction = (response) => {
+					setBusiness(response.data);
+				};
+				const errorFunction = (error) => {
+					console.log(error);
+				};
+				putAPI(url, requestBody, thenFunction, errorFunction);
+			} else {
+				setmodalHL(true);
+			}
+		};
+        const businessHighlightedProducts = {
+			highlightedProducts: business.highlightedProducts,
+			handleHighlightedProduct: handleHighlightedProduct,
+		};
         return (
             <div className='container-fluid'>
                 <div className="row p-0">
@@ -103,8 +171,30 @@ const BusinessProducts = () => {
                     </div>
                 </div>
                 <div className="row p-0 justify-content-center">
+                <div className='col-11 col-md-8 pb-5 d-flex flex-wrap justify-content-around align-items-stretch '>
+								{business.products
+									.map((product) => {
+										return (
+											<ProductCard2
+												key={uuidv4()}
+												product={product}
+												businessNameEncoded={businessNameEncoded}
+												currency={currency}
+												cart={cart}
+												setBusiness={setBusiness}
+												handleModal={handleModal}
+												owner={owner}
+												userSaved={userSaved}
+												handleSavedProductStatus={handleSavedProductStatus}
+												businessHighlightedProducts={
+													businessHighlightedProducts
+												}
+											/>
+										);
+									})}
+							</div>
 
-                {window.innerWidth < 450 ? 
+                {/* {window.innerWidth < 450 ? 
                     <div className="col-12 d-flex flex-wrap justify-content-center align-items-stretch ">
                         {business.products.map(product =>{
                             return <ProductCard key={uuidv4()} currency={currency} product={product} setBusiness={setBusiness} businessNameEncoded={businessNameEncoded} handleModal={handleModal} owner={owner}/>
@@ -114,7 +204,7 @@ const BusinessProducts = () => {
                         {business.products.map(product =>{
                             return <ProductCardDesktop key={uuidv4()} currency={currency} product={product} setBusiness={setBusiness} businessNameEncoded={businessNameEncoded} handleModal={handleModal} owner={owner}/>
                         })}
-                    </div>}
+                    </div>} */}
                     
                 </div>
                 <Modal
@@ -139,6 +229,34 @@ const BusinessProducts = () => {
                 </Button>
                 </Modal.Footer>
             </Modal>
+            {/* MODAL FOR LIMIT HIGHLIGHTED */}
+				<Modal
+					show={modalHL}
+					onHide={() => setmodalHL(false)}
+					backdrop='static'
+					keyboard={false}
+				>
+					<Modal.Header closeButton>
+						<Modal.Title>{languages[0][lang].business.mHLtitle}</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						{`${languages[0][lang].business.mHLtext1} (${limitHL}).`} <br />
+						{languages[0][lang].business.mHLtext2}
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant='secondary' onClick={() => setmodalHL(false)}>
+							{languages[0][lang].business.btnCancel}
+						</Button>
+						<Button
+							variant='danger'
+							onClick={() => {
+								navigate(`/${businessNameEncoded}/memberships`);
+							}}
+						>
+							{languages[0][lang].business.btnMembership}
+						</Button>
+					</Modal.Footer>
+				</Modal>
             </div>
 
         )
