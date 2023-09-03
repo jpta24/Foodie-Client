a2cd7b458663a00e965150532a20edd4ea29118a
Jpta24 <jeanpierretorres@gmail.com>
2023-09-03 22:11:52 +0200

    test mail

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,31 @@
-
-import DashboardBusinessChart from '../components/DashboardBusinessChart';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 function Test() {
+	const requestBody = {
+		nombre:'yo',correo:'jeanpierretorres@gmail.com'
+	}
+
+	const postAPI = async () => {
+		try {
+			await axios.post(
+				`https://pink-clumsy-gosling.cyclic.app/mails/webfoddys`,
+				requestBody
+			);
+		} catch (error) {
+			console.error(error)
+		}
+	};
 	return (
 		<div className='flex-grow-1'>
-			<DashboardBusinessChart/>
+		<Button
+		variant='outline-primary'
+		size='lg'
+		className='col-11 col-lg-8 my-2 '
+		onClick={postAPI}
+	>
+		Send mail
+	</Button>
 		</div>				
 	);
 }
